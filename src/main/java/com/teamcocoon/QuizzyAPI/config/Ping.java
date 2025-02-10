7db857571e22e39618ba683c8a227ef596c3aa4c
felix <badjisidyfelix@gmail.com>
2025-02-10 13:42:22 +0100

    Issue Ping Done

--- src/main/java/com/teamcocoon/QuizzyAPI/config/Ping.java
+++ src/main/java/com/teamcocoon/QuizzyAPI/config/Ping.java
@@ -1,13 +1,64 @@
 package com.teamcocoon.QuizzyAPI.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/ping")
+@RestController
+@RequestMapping("api/ping")
 public class Ping {
-    @GetMapping()
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("Ok");
+
+    @GetMapping
+    public ResponseEntity<Object> ping() {
+        try {
+            return ResponseEntity.ok().body(new PingResponse("OK", new PingDetails("OK")));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new PingResponse("KO", new PingDetails("KO")));
+        }
+    }
+
+    public static class PingResponse {
+        private String status;
+        private PingDetails details;
+
+        public PingResponse(String status, PingDetails details) {
+            this.status = status;
+            this.details = details;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public PingDetails getDetails() {
+            return details;
+        }
+
+        public void setDetails(PingDetails details) {
+            this.details = details;
+        }
+    }
+
+    public static class PingDetails {
+        private String database;
+
+        public PingDetails(String database) {
+            this.database = database;
+        }
+
+        public String getDatabase() {
+            return database;
+        }
+
+        public void setDatabase(String database) {
+            this.database = database;
+        }
     }
 }
