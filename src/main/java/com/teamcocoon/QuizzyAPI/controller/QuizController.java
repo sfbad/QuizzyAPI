b1b382b94ba58c3ab98e666c3c41f4193f87b720
massiferrouk <massiferrouk3@gmail.com>
2025-02-11 14:56:04 +0100

    Create Quiz

--- src/main/java/com/teamcocoon/QuizzyAPI/controller/QuizController.java
+++ src/main/java/com/teamcocoon/QuizzyAPI/controller/QuizController.java
@@ -1,4 +1,45 @@
 package com.teamcocoon.QuizzyAPI.controller;
 
+
+import com.teamcocoon.QuizzyAPI.dtos.UserRequestDto;
+import com.teamcocoon.QuizzyAPI.model.Quiz;
+import com.teamcocoon.QuizzyAPI.service.QuizService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/")
 public class QuizController {
+
+    private final QuizService quizService;
+
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+
+    @PostMapping("/quiz")
+    public ResponseEntity<Void> createQuiz(@RequestBody Quiz quiz){
+        Quiz savedQuiz = quizService.saveQuiz(quiz);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedQuiz.getQuizId())
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/teamcocoon/QuizzyAPI/service/QuizService.java
+++ src/main/java/com/teamcocoon/QuizzyAPI/service/QuizService.java
@@ -1,4 +1,17 @@
 package com.teamcocoon.QuizzyAPI.service;
 
+import com.teamcocoon.QuizzyAPI.model.Quiz;
+import com.teamcocoon.QuizzyAPI.repositories.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuizService {
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    public Quiz saveQuiz(Quiz quiz) {
+        return quizRepository.save(quiz);
+    }
 }
