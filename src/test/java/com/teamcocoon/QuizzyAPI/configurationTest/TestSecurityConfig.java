c57bc14d01206d1f1b98467c5eedbc31d0a35608
Sidy Felix Badji <86934701+sfbad@users.noreply.github.com>
2025-02-13 12:29:11 +0100

    Adding new Question

--- src/test/java/com/teamcocoon/QuizzyAPI/configurationTest/TestSecurityConfig.java
+++ src/test/java/com/teamcocoon/QuizzyAPI/configurationTest/TestSecurityConfig.java
@@ -2,6 +2,7 @@ package com.teamcocoon.QuizzyAPI.configurationTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
@@ -11,7 +12,7 @@ public class TestSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .csrf(csrf -> csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
