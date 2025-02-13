e8868de1e9ed0a99f2b81438111e1bf6baec5c95
raniamasdoua <masdouarania02@gmail.com>
2025-02-13 16:34:28 +0100

    test issue5

--- src/test/java/com/teamcocoon/QuizzyAPI/controller/QuizControllerTest.java
+++ src/test/java/com/teamcocoon/QuizzyAPI/controller/QuizControllerTest.java
@@ -10,6 +10,7 @@ import com.teamcocoon.QuizzyAPI.model.Quiz;
 import com.teamcocoon.QuizzyAPI.model.User;
 import com.teamcocoon.QuizzyAPI.repositories.UserRepository;
 import com.teamcocoon.QuizzyAPI.service.QuizService;
+import com.teamcocoon.QuizzyAPI.utils.UserUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,6 +36,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
@@ -53,7 +55,7 @@ class QuizControllerTest {
 
     @Test
     void createQuiz_returns201WithLocation() throws Exception {
-        QuizDto quiz = new QuizDto(-1L, "New quizz");
+        QuizDto quiz = new QuizDto(-1L, "New quizz1");
         UserRequestDto user = new UserRequestDto("Test");
         mockMvc.perform(post("/api/users")
                         .with(jwt().jwt(jwt -> jwt.claim("sub", "testUser")))
@@ -69,12 +71,8 @@ class QuizControllerTest {
 
     @Test
     void createQuiz_returns201WithLocation2() throws Exception {
-        QuizDto quiz = new QuizDto(-1L, "New quizz");
-        UserRequestDto user = new UserRequestDto("Test");
-        mockMvc.perform(post("/api/users")
-                        .with(jwt().jwt(jwt -> jwt.claim("sub", "testUser")))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)));
+        QuizDto quiz = new QuizDto(-1L, "New quizz2");
+        UserUtils.createUserIfNotExists("testUser");
 
         mockMvc.perform(post("/api/quiz")
                         .with(jwt().jwt(jwt -> jwt.claim("sub", "testUser")))
@@ -83,4 +81,20 @@ class QuizControllerTest {
                 .andExpect(status().isCreated());
 
     }
+
+    @Test
+    void getListQuiz_returnsListOfQuizzes() throws Exception {
+        // Arrange: Créer un quiz pour un utilisateur donné
+        createQuiz_returns201WithLocation();
+        createQuiz_returns201WithLocation2();
+
+        // Act & Assert: Vérifier que l'utilisateur peut récupérer la liste des quiz
+        mockMvc.perform(get("/api/quiz")
+                        .with(jwt().jwt(jwt -> jwt.claim("sub", "testUser"))))
+                .andExpect(status().isOk()) // Vérifie que la réponse est 200 OK
+                .andExpect(jsonPath("$.data").isArray()) // Vérifie que la réponse contient un tableau JSON
+                .andExpect(jsonPath("$.data[0].title").value("New quizz1")) // Vérifie que le quiz attendu est dans la réponse
+                .andExpect(jsonPath("$.data[1].title").value("New quizz2"));
+    }
+
 }
--- src/test/java/com/teamcocoon/QuizzyAPI/utils/UserUtils.java
+++ src/test/java/com/teamcocoon/QuizzyAPI/utils/UserUtils.java
@@ -2,21 +2,25 @@ package com.teamcocoon.QuizzyAPI.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teamcocoon.QuizzyAPI.dtos.UserRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@Configuration
 public class UserUtils {
-    private MockMvc mvc;
-    private ObjectMapper mapper = new ObjectMapper();
+    @Autowired(required = false)  // this will be injected if UserUtils is used in a Spring Boot application
+    private static MockMvc mvc ;
+    private static ObjectMapper mapper = new ObjectMapper();
 
     UserUtils(MockMvc mvc){
         this.mvc = mvc;
     }
 
-    void createUserIfNotExists(String username) throws Exception {
+    public static void createUserIfNotExists(String username) throws Exception {
         UserRequestDto user = new UserRequestDto(username);
         mvc.perform(post("/api/users")
                 .with(jwt().jwt(jwt -> jwt.claim("sub", "testUser")))
