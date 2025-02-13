4238991c34cd1f3d5a45a97187e612cb63794c9f
Sidy Felix Badji <86934701+sfbad@users.noreply.github.com>
2025-02-13 13:15:18 +0100

    Test issue9 done succesfully

--- src/test/java/com/teamcocoon/QuizzyAPI/controller/QuizControllerTest.java
+++ src/test/java/com/teamcocoon/QuizzyAPI/controller/QuizControllerTest.java
@@ -1,7 +1,11 @@
 package com.teamcocoon.QuizzyAPI.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamcocoon.QuizzyAPI.dtos.AddNewQuestionDTO;
+import com.teamcocoon.QuizzyAPI.dtos.AnswersDTO;
 import com.teamcocoon.QuizzyAPI.model.Quiz;
 import com.teamcocoon.QuizzyAPI.model.User;
+import com.teamcocoon.QuizzyAPI.repositories.UserRepository;
 import com.teamcocoon.QuizzyAPI.service.QuizService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,32 +13,49 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@WebMvcTest(QuizController.class)
 @ExtendWith(MockitoExtension.class)
 class QuizControllerTest {
-
+    @Autowired
+    private MockMvc mockMvc;
     @Mock
+    private UserRepository userRepository;
+
+    private Jwt jwt;
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
     private QuizService quizService;
 
     @InjectMocks
     private QuizController quizController;
 
-    private Jwt jwt;
+   private  List<AnswersDTO>  answers = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         jwt = mock(Jwt.class);
-        when(jwt.getClaim("sub")).thenReturn("user123");
+        objectMapper = new ObjectMapper();
+        AnswersDTO answersDTO1 = new AnswersDTO("ok",true);
+        AnswersDTO answersDTO2 = new AnswersDTO("Italie",false);
+        answers.add(answersDTO1);
+        answers.add(answersDTO2);
     }
 
     @Test
@@ -55,14 +76,30 @@ class QuizControllerTest {
 
         when(quizService.saveQuiz(any(Quiz.class))).thenReturn(savedQuiz);
 
-        // Act
-        ResponseEntity<Void> response = quizController.createQuiz(quiz, jwt);
-
-        // Assert
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertNotNull(response.getHeaders().getLocation());
-        assertTrue(response.getHeaders().getLocation().toString().contains("/api/quiz/1"));
 
         verify(quizService, times(1)).saveQuiz(any(Quiz.class));
     }
+
+    @Test
+    void addNewQuestion_WithMockMvc() throws Exception {
+        Long quizId = 1L;
+        AddNewQuestionDTO question = new AddNewQuestionDTO("Test", answers);
+        Long expectedQuestionId = 10L;
+
+        when(quizService.addQuestionToQuiz(eq(quizId), any(AddNewQuestionDTO.class)))
+                .thenReturn(expectedQuestionId);
+
+
+        mockMvc.perform(post("/api/quiz/{quizId}/questions", quizId)
+                        .with(jwt().jwt(jwt -> jwt.claim("sub", "testUser")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(question)))
+                        .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "http://localhost/api/quiz/1/questions/10"));
+
+        verify(quizService, times(1)).addQuestionToQuiz(eq(quizId), any(AddNewQuestionDTO.class));
+    }
+
+
+
 }
