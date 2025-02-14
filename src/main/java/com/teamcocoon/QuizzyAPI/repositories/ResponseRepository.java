5d054ce03bb94e3fdb55ba8c95711294ee8015a8
Sidy Felix Badji <86934701+sfbad@users.noreply.github.com>
2025-02-14 14:51:21 +0100

    Implementation de Issue 11

--- src/main/java/com/teamcocoon/QuizzyAPI/repositories/ResponseRepository.java
+++ src/main/java/com/teamcocoon/QuizzyAPI/repositories/ResponseRepository.java
@@ -3,6 +3,7 @@ package com.teamcocoon.QuizzyAPI.repositories;
 import com.teamcocoon.QuizzyAPI.model.Question;
 import com.teamcocoon.QuizzyAPI.model.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,8 @@ import java.util.List;
 
 @Repository
 public interface ResponseRepository extends JpaRepository<Response, Long> {
+    @Modifying
+    @Query("DELETE FROM Response r WHERE r.question.questionId = :questionId")
     void deleteAllByQuestion(Question question);
 
 }
