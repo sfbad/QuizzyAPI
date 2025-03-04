30335195673e51618f792b60eb7bd699877d5d80
Sidy Felix Badji <86934701+sfbad@users.noreply.github.com>
2025-03-04 10:25:38 +0100

    Performing Test

--- src/test/java/com/teamcocoon/QuizzyAPI/controller/QuizControllerTest.java
+++ src/test/java/com/teamcocoon/QuizzyAPI/controller/QuizControllerTest.java
@@ -9,6 +9,7 @@ import com.teamcocoon.QuizzyAPI.utils.TestUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -37,6 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 )
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 @TestPropertySource("classpath:application-test.properties")
 class QuizControllerTest {
 
